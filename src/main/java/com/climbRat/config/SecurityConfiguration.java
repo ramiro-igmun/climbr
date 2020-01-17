39a4963a50a6c0ad85159311cc1b4765e2e95244
david <david>
2020-01-17 20:30:11 +0100

    add swagger and constructor injection

--- src/main/java/com/climbRat/config/SecurityConfiguration.java
+++ src/main/java/com/climbRat/config/SecurityConfiguration.java
@@ -1,25 +1,30 @@
-package com.climbRat.security;
+package com.climbRat.config;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @Profile("dev")
-public class SecurityConfigurationDevelopment extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-  @Autowired
-  UserDetailsService climbRatUserDetailsService;
+  private final UserDetailsService climbRatUserDetailsService;
+
+  public SecurityConfiguration(UserDetailsService climbRatUserDetailsService) {
+    this.climbRatUserDetailsService = climbRatUserDetailsService;
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -38,12 +43,19 @@ public class SecurityConfigurationDevelopment extends WebSecurityConfigurerAdapt
     auth.userDetailsService(climbRatUserDetailsService);
   }
 
+  @Override
+  public void configure(WebSecurity web) {
+    web.ignoring()
+            .antMatchers(HttpMethod.OPTIONS, "/**")
+            .antMatchers("/swagger-ui/index.html")
+            .antMatchers("/test/**");
+  }
+
+
   @Bean
   public PasswordEncoder passwordEncoder(){
     return NoOpPasswordEncoder.getInstance();
   }
-
-
 
 }
 
